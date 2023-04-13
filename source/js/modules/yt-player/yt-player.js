dccfbda5e249447a16b1323144e239c681c3d324
afamaria <marie.afanasyeva@gmail.com>
2023-04-13 14:09:23 +0300

    Fix after testing v1.2

--- source/js/modules/yt-player/yt-player.js
+++ source/js/modules/yt-player/yt-player.js
@@ -6,7 +6,7 @@ const initVideo = () => {
     btn.addEventListener('click', (evt) => {
       evt.preventDefault();
       wrapper.insertAdjacentHTML('afterbegin','<iframe class="intro__video" src="https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1" title="YouTube видео" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-      setTimeout(() => wrapper.classList.add('intro__video-wrapper--play'), "1000");
+      wrapper.classList.add('intro__video-wrapper--play');
     });
   } else {
   }
